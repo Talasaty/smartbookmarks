dd0812a849668be19e578fafd189b8069bc8ada4
Juan Luis <mnjuanluis@gmail.com>
2019-07-18 19:21:09 +0200

    añade formik a los formularios creados

--- src/views/Home/Home.view.tsx
+++ src/views/Home/Home.view.tsx
@@ -10,88 +10,102 @@ import search from '$icons/search.svg'
 import test01 from '$imgs/test01.jpg'
 import hamburger from '$icons/hamburger.svg'
 import { HamburgerButton } from './Home.styles'
-import { Button } from "$components/Button";
+import { Button } from '$components/Button'
+import { Formik, Form } from 'formik'
 interface State {
   showMenu: boolean
 }
 export class Home extends React.Component<State> {
   state = {
-    showMenu: false
+    showMenu: false,
   }
 
-  hamburgerMenuIsClicked = () => this.setState({showMenu: !this.state.showMenu})
+  hamburgerMenuIsClicked = () =>
+    this.setState({ showMenu: !this.state.showMenu })
   render() {
     const exampleText =
       'Lorem ipsum dolor sit amet consectetur adipiscing elit asdsfds'
     const NameCategory = 'Lorem ipsum dolor sit '
     const { showMenu } = this.state
     return (
-      <>
-        <HeaderHome>
-          <HamburgerButton >
-          <Button onClick={() => this.hamburgerMenuIsClicked()}>
-
-            <img src={hamburger} />
-          </Button>
-          </HamburgerButton>
-          <Input
-            iconRight={search}
-            backgroundColor="blue"
-            borderColor="blue"
-            color="gray"
-            type="restricted"
-          />
-        </HeaderHome>
-        <MainHome>
-          <AsideHome show={showMenu}>
-            <Category Name={NameCategory} />
-            <Category Name={NameCategory} />
-            <Category Name={NameCategory} />
-            <Category Name={NameCategory} />
-            <Category Name={NameCategory} />
-            <Category Name={NameCategory} />
-            <Category Name={NameCategory} />
-          </AsideHome>
-          <SectionHome>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-            <Card>
-              <Picture img={test01} />
-              <Description DescriptionText={exampleText} />
-            </Card>
-          </SectionHome>
-        </MainHome>
-      </>
+      <Formik
+        initialValues={{ browser: 'coso' }}
+        onSubmit={(values, actions) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            actions.setSubmitting(false)
+          }, 1000)
+        }}
+        render={() => (
+          <Form>
+            <HeaderHome>
+              <HamburgerButton>
+                <Button onClick={() => this.hamburgerMenuIsClicked()}>
+                  <img src={hamburger} />
+                </Button>
+              </HamburgerButton>
+              <Input
+                iconRight={search}
+                backgroundColor="blue"
+                borderColor="blue"
+                color="gray"
+                typeInput="restricted"
+                type="text"
+                name="browser"
+              />
+            </HeaderHome>
+            <MainHome>
+              <AsideHome show={showMenu}>
+                <Category Name={NameCategory} />
+                <Category Name={NameCategory} />
+                <Category Name={NameCategory} />
+                <Category Name={NameCategory} />
+                <Category Name={NameCategory} />
+                <Category Name={NameCategory} />
+                <Category Name={NameCategory} />
+              </AsideHome>
+              <SectionHome>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+                <Card>
+                  <Picture img={test01} />
+                  <Description DescriptionText={exampleText} />
+                </Card>
+              </SectionHome>
+            </MainHome>
+          </Form>
+        )}
+      />
     )
   }
 }
